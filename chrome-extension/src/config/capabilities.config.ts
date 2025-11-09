3147116ae832434d2be4e359cd238e526d5fc0b3
Igor Yuzkiv <igor97w@gmail.com>
2025-11-09 14:56:06 +0200

    chrome-extension-v2

--- chrome-extension/src/config/capabilities.config.ts
+++ chrome-extension/src/config/capabilities.config.ts
@@ -1,7 +1,4 @@
-import { crmFieldsCapabilityPortFactory } from '@/shared/integrations/zoho-crm/capabilities/crm.fields.capability.ts'
-import { crmFunctionsCapabilityPortFactory } from '@/shared/integrations/zoho-crm/capabilities/crm.functions.capability.ts'
-import { crmModulesCapabilityPortFactory } from '@/shared/integrations/zoho-crm/capabilities/crm.modules.capability.ts'
-import { crmWorkflowCapabilityPortFactory } from '@/shared/integrations/zoho-crm/capabilities/crm.workflows.capability.ts'
+import ZohoCrmCapabilities from '@/shared/integrations/zoho-crm/capabilities'
 import type { ProviderCapability } from '@/entities/capability/capability.types.ts'
 import type { ProviderType } from '@/entities/provider/provider.types.ts'
 
@@ -15,32 +12,6 @@ export const CapabilityType = {
 export const CAPABILITY_DEFAULT_ICON = 'carbon:undefined'
 
 export const CapabilitiesRegister: Record<ProviderType, ProviderCapability[]> = {
-    crm: [
-        {
-            type: CapabilityType.FUNCTIONS,
-            title: 'Zoho CRM Functions',
-            icon: 'material-symbols:function',
-            portFactory: crmFunctionsCapabilityPortFactory,
-        },
-        {
-            type: CapabilityType.WORKFLOWS,
-            title: 'Zoho CRM Workflows',
-            icon: 'mdi:workflow',
-            portFactory: crmWorkflowCapabilityPortFactory,
-        },
-        {
-            type: CapabilityType.MODULES,
-            title: 'Zoho CRM Modules Metadata',
-            icon: 'streamline-sharp:module',
-            portFactory: crmModulesCapabilityPortFactory,
-        },
-        {
-            type: CapabilityType.FIELDS,
-            title: 'Zoho CRM Modules Fields Metadata',
-            icon: 'hugeicons:list-setting',
-            portFactory: crmFieldsCapabilityPortFactory,
-            hideInMenu: true,
-        },
-    ],
+    crm: ZohoCrmCapabilities,
     finance: [],
 }
