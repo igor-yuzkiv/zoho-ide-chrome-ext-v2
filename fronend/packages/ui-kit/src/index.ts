421a2ec229a75dafef7c6d31c1b9e34107ff2011
Igor Yuzkiv <igor97w@gmail.com>
2025-11-16 16:19:49 +0200

    packages/ui-kit/src

--- fronend/packages/ui-kit/src/index.ts
+++ fronend/packages/ui-kit/src/index.ts
@@ -2,8 +2,6 @@ import primeVuePlugin from './plugins/primevue/prime-vue.plugin.ts'
 import './styles/ui-kit.css'
 import type { App } from 'vue'
 
-export * from './components/index.ts'
-
 export function registerUiKitPlugins(app: App) {
     primeVuePlugin(app)
 }
