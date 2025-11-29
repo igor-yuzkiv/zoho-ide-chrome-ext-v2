5165a1b41ab495454d7c2e5805cb0c2c6bfc36a2
Igor Yuzkiv <igor97w@gmail.com>
2025-11-29 10:11:42 +0200

    feature/knowledge-base/main

--- fronend/packages/backend-api/src/user/requests/delete.user.request.ts
+++ fronend/packages/backend-api/src/user/requests/delete.user.request.ts
@@ -1,5 +1,5 @@
 import { apiClient } from '../../api.client.ts'
-import { DeleteUserByIdResponse } from '../types'
+import type { DeleteUserByIdResponse } from '../types'
 
 export async function deleteUserByIdRequest(userId: string): Promise<DeleteUserByIdResponse> {
     return apiClient.delete(`/users/${userId}`).then((response) => response.data)
