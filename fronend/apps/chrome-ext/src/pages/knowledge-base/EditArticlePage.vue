107dca212ef29f89d58d463ec98d703aabc89ebf
Igor Yuzkiv <igor97w@gmail.com>
2025-12-10 19:38:28 +0200

    chrome-extension: enhance knowledge base article routing and editing functionality

---
+++ fronend/apps/chrome-ext/src/pages/knowledge-base/EditArticlePage.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useRouteParams } from '@vueuse/router'
+import { ArticleEditView, useKbItemDetailsQuery, useUpdateKbItem } from '@zoho-ide/knowledge-base'
+import { Button } from 'primevue'
+import { Icon } from '@iconify/vue'
+import { AppRouteName } from '@/app/router/app-routes.ts'
+
+const itemId = useRouteParams<string>('itemId')
+const { data } = useKbItemDetailsQuery(itemId)
+const { formData, submit } = useUpdateKbItem(data)
+</script>
+
+<template>
+    <ArticleEditView v-if="data" v-model="formData">
+        <template #header-actions>
+            <Button
+                size="small"
+                text
+                severity="secondary"
+                as="router-link"
+                :to="{ name: AppRouteName.knowledgeBaseArticleDetails, params: { itemId: data.id } }"
+            >
+                <Icon icon="material-symbols:close" />
+                <span>Cancel</span>
+            </Button>
+            <Button size="small" text @click="submit">
+                <Icon icon="material-symbols:save" />
+                <span>Save</span>
+            </Button>
+        </template>
+    </ArticleEditView>
+</template>
+
+<style scoped></style>
