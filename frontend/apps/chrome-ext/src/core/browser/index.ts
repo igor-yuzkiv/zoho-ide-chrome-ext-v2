99dad02eba7900ab8cb89076e17d676b6c95c8df
Igor Yuzkiv <igor97w@gmail.com>
2025-12-27 13:29:06 +0200

    remove old global search

--- frontend/apps/chrome-ext/src/core/browser/index.ts
+++ frontend/apps/chrome-ext/src/core/browser/index.ts
@@ -1,4 +1,4 @@
 export * from './browser.types.ts'
-export * from './injection/injection.utils.ts'
-export * from './injection/zoho-crm-injection-request.ts'
+export * from '@/core/browser/script-injection/injection.utils.ts'
+export * from '@/core/browser/script-injection/zoho-crm-injection-request.ts'
 export * from './store/useBrowserTabsStore.ts'
